fbd9089e1edd89f1571f58f5689ee00813edb1d9
rizki <rizki19@github.com>
2020-09-25 02:27:43 +0700

    Updates

---
+++ precache-manifest.70637bb795f03d0efc404f8e460407f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ca2a416d23c131a96ae20d261e7bd9",
+    "url": "/52de5a0f22001b919bf98a1fad00fde8c38be9de/index.html"
+  },
+  {
+    "revision": "734f00f69e5117be5485",
+    "url": "/52de5a0f22001b919bf98a1fad00fde8c38be9de/static/css/main.4f3aab64.chunk.css"
+  },
+  {
+    "revision": "7e1361e1451bba045e98",
+    "url": "/52de5a0f22001b919bf98a1fad00fde8c38be9de/static/js/2.3af6e3cb.chunk.js"
+  },
+  {
+    "revision": "a3deeb6dc46d8547858faa3092c59296",
+    "url": "/52de5a0f22001b919bf98a1fad00fde8c38be9de/static/js/2.3af6e3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734f00f69e5117be5485",
+    "url": "/52de5a0f22001b919bf98a1fad00fde8c38be9de/static/js/main.e574beb2.chunk.js"
+  },
+  {
+    "revision": "bde7e0c0a703082189f8",
+    "url": "/52de5a0f22001b919bf98a1fad00fde8c38be9de/static/js/runtime-main.0bfc583b.js"
+  }
+]);
